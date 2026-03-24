72c2c0bce5ed35d8f778e6bc081479512ddb82f5
Moon <svm52@txstate.edu>
2026-03-23 23:58:14 -0500

    update

--- app.py
+++ app.py
@@ -6,6 +6,7 @@ app = Flask(__name__)
 app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:////Users/moon/Desktop/Code/Calorie counter/instance/calories.db'
 db = SQLAlchemy(app)
 #jxhchcdhhd
+#jhdhasdggdgdgg
 
 class foodEntry(db.Model):
     id = db.Column(db.Integer, primary_key = True)
